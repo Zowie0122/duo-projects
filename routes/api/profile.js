727ef1c916a1595ef5dad7b085151bcdc4b26119
Kapakahi <rubbishman@gmail.com>
2020-05-10 16:57:44 +0900

    created input form for CV

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const axios = require("axios");
+// const axios = require('axios');
 const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
