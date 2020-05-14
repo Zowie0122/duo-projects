c9d39e05093c7c511d72fbbdd9d015055622b073
Zowie <51642770+Zowie0122@users.noreply.github.com>
2020-05-14 10:33:50 +0900

    fixed the auth middleware bug

--- routes/api/users.js
+++ routes/api/users.js
@@ -30,6 +30,7 @@ router.post(
       "Please enter a password with 6 or more characters",
     ).isLength({ min: 6 }),
   ],
+
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -72,7 +73,7 @@ router.post(
       jwt.sign(
         payload,
         config.get("jwtSecret"),
-        { expiresIn: 360000 },
+        { expiresIn: 36000000 },
         (err, token) => {
           if (err) throw err;
           res.json({
@@ -98,11 +99,17 @@ module.exports = router;
 //   "email": "minjiayi@gmail.com"
 //   }
 
-
-
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWViNzUzMWI5MThlMzcwOWU2NjRjNGE3In0sImlhdCI6MTU4OTA3MjY2OCwiZXhwIjoxNTg5NDMyNjY4fQ.fRHrWaYfZRPJkOX16m9GAhLYmwuaPz1x2mxzk-VylZ4
 // {
 //   "name":"Hi",
 //   "password": "sssssss",
 //   "email": "hi@gmail.com"
+//   }
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWViYzkyNThjYTRjNTYwNmIyNDYxNzY1In0sImlhdCI6MTU4OTQxNjUzNywiZXhwIjoxNTg5Nzc2NTM3fQ.j_hIVgvE_SrI3PT1voZxKpdd9XjbXyRF1KXHuN710cc
+
+// {
+//   "name":"Hirr",
+//   "password": "sssssss",
+//    "email": "hidd@gmail.com"
 //   }
