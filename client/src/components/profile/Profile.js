28771d8cabbf650913683095368b43efd90c7f24
Zowie <51642770+Zowie0122@users.noreply.github.com>
2020-05-17 17:05:32 +0900

    cv generator

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
@@ -8,11 +8,66 @@ import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
 import { getProfileById } from "../../actions/profile";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
   useEffect(() => {
     getProfileById(match.params.id);
   }, [getProfileById, match.params.id]);
+
+  const [pdfContent, setPdfContent] = useState("");
+
+  const jsPDFGenerator = () => {
+    if (profile) {
+      let doc = new jsPDF("p", "pt");
+      var text = [];
+      const list = [];
+      list.push(profile.user.name);
+
+      list.push("Skills");
+
+      profile.skills.forEach((element) => {
+        list.push(element);
+      });
+
+      list.push("Education");
+      profile.education.forEach((ele) => {
+        for (let key in ele) {
+          console.log(key);
+          console.log(ele[key]);
+          if (key !== "_id") {
+            list.push(`${key.toString()}:${ele[key]}`);
+          }
+        }
+
+        console.log(ele);
+      });
+
+      list.push("Experience");
+
+      profile.experience.forEach((ele) => {
+        for (let key in ele) {
+          console.log(key);
+          console.log(ele[key]);
+          if (key !== "_id") {
+            list.push(`${key.toString()}:${ele[key]}`);
+          }
+        }
+
+        console.log(ele);
+      });
+
+      for (var i of list) {
+        text.push(i);
+      }
+      doc.text(text, 20, 20);
+
+      doc.save("generated.pdf");
+    }
+  };
+
+  console.log(profile);
 
   return (
     <Fragment>
@@ -65,6 +120,7 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
               )}
             </div>
           </div>
+          <button onClick={jsPDFGenerator}>Download CV</button>
         </Fragment>
       )}
     </Fragment>
